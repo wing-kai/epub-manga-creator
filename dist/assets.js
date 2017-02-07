a0c16399de585f1009c9dd2163a5a22a503c0446
wing-kai <cyj9385@hotmail.com>
2017-02-08 00:08:04 +0800

    update

--- dist/assets.js
+++ dist/assets.js
@@ -1,1 +1,1 @@
-window.env="production";window.WEBPACK_ASSETS={"commons":{"js":"dist/commons.2e0892226a855675a830.bundle.js"},"index":{"js":"dist/index.2e0892226a855675a830.bundle.js"}}
+window.env="development";window.WEBPACK_ASSETS={"commons":{"js":"/dist/commons.010959ff0ca77d4f4ac3.bundle.js"},"index":{"js":"/dist/index.010959ff0ca77d4f4ac3.bundle.js"}}
