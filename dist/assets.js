5c7fa26d2e11ee92602bd64802d0cce397b08e56
wing-kai <cyj9385@hotmail.com>
2017-02-19 03:59:39 +0800

    update ui 2.0

--- dist/assets.js
+++ dist/assets.js
@@ -1,1 +1,1 @@
-window.env="production";window.WEBPACK_ASSETS={"commons":{"js":"dist/commons.893dd160e24de3a237e7.bundle.js"},"index":{"js":"dist/index.893dd160e24de3a237e7.bundle.js"}}
+window.env="production";window.WEBPACK_ASSETS={"commons":{"js":"dist/commons.aac952487d08a071c810.bundle.js"},"index":{"js":"dist/index.aac952487d08a071c810.bundle.js"}}
